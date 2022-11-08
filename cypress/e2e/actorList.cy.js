eebd56b159085dd05f9fe1c2f51ea6e37217ac95
Fan Yikun <LuMingJun71543@outlook.com>
2022-11-08 19:54:19 +0000

    actor list part tested for first time

---
+++ cypress/e2e/actorList.cy.js
@@ -0,0 +1,50 @@
+let actors;
+let movies;
+describe("Check review content", () => { //最外围的主要测试，
+    before(() => {
+        cy.request(
+            `https://api.themoviedb.org/3/discover/movie?api_key=${Cypress.env("TMDB_KEY")}&language=en-US&include_adult=false&include_video=false&page=1`
+        ).its("body")
+            .then((response) => {
+                movies = response.results;
+            });
+    });
+    before(() => {
+        cy.request(
+            `https://api.themoviedb.org/3/movie/${movies[4].id}/credits?api_key=${Cypress.env("TMDB_KEY")}&language=en-US`
+        ).its("body")
+            .then((response) => {
+                actors = response.cast;
+            });
+    });
+
+    beforeEach(() => {
+        cy.visit(`/movies/${movies[4].id}`);
+        cy.wait(1000);
+    })
+
+    describe("dynamic data in actor list is correct", () => {
+
+        it("test the length of actor list", () => {
+            cy.get(".MuiBox-root").should("have.length", actors.length + 1);//这个总是多出来的一个是谁？
+        })
+
+        // it("test navigate to the actor's detail information ", () => {
+        //     cy.get(".MuiCardContent-root").eq(0).click();
+        //     cy.url().should("include", `/actors/${actors[0].id}`)
+        // })//测试跳转的成功不成功
+
+        it("test the name and character of certain actor", () => {
+            cy.get(".MuiCardContent-root").eq(0).contains(actors[0].name);
+            cy.get(".MuiCardContent-root").eq(0).contains(actors[0].character);
+        })
+    });
+
+    describe("static data in actor list is correct", () => {
+        it("test the title and character of certain actor", () => {
+            cy.get("h3").contains("Main Characters");
+        })
+    });
+});
+
+
