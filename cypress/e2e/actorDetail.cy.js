98bba586b88989c11dec210393baea7cd80a58a5
Fan Yikun <LuMingJun71543@outlook.com>
2022-11-08 23:52:53 +0000

    actor detail page tested

---
+++ cypress/e2e/actorDetail.cy.js
@@ -0,0 +1,105 @@
+let popular;
+let movies;
+let actorDetails;
+let externalID;
+let actorCredits;
+describe("Check actor details page", () => {
+    before(() => {
+        cy.request(
+            `https://api.themoviedb.org/3/person/popular?api_key=${Cypress.env("TMDB_KEY")}&language=en-US&page=1`
+        ).its("body")
+            .then((response) => {
+                popular = response.results;
+            });
+    });
+    before(() => {
+        cy.request(
+            `https://api.themoviedb.org/3/discover/movie?api_key=${Cypress.env("TMDB_KEY")}&language=en-US&include_adult=false&include_video=false&page=1`
+        ).its("body")
+            .then((response) => {
+                movies = response.results;
+            });
+    });
+    before(() => {
+        cy.request(
+            `https://api.themoviedb.org/3/person/${popular[4].id}?api_key=${Cypress.env("TMDB_KEY")}&language=en-US`
+        ).its("body")
+            .then((response) => {
+                actorDetails = response;
+            });
+    });
+    before(() => {
+        cy.request(
+            `https://api.themoviedb.org/3/person/${popular[4].id}/external_ids?api_key=${Cypress.env("TMDB_KEY")}&language=en-US`
+        ).its("body")
+            .then((response) => {
+                externalID = response;
+            });
+    });
+    before(() => {
+        cy.request(
+            `https://api.themoviedb.org/3/person/${popular[4].id}/movie_credits?api_key=${Cypress.env("TMDB_KEY")}&language=en-US`
+        ).its("body")
+            .then((response) => {
+                actorCredits = response.cast;
+            });
+    });
+
+    describe("able to navigate to actor detail page", () => {
+        beforeEach(() => {
+            cy.visit(`/people/popular`);
+            cy.wait(1000);
+        })
+        it("navigate to correct actor detail page", () => {
+            cy.get(".MuiTypography-h6").eq(5).click();
+            cy.url().should("include", `/actors/${popular[4].id}`);
+        })
+    });
+
+    describe("basic data in actor detail page is correct", () => {
+        beforeEach(() => {
+            cy.visit(`/actors/${popular[4].id}`);
+            cy.wait(1000);
+        })
+        it("test the basic info of actor", () => {
+            cy.get(".MuiTypography-body1").contains(actorDetails.birthday);
+            cy.get(".MuiTypography-body1").contains(actorDetails.known_for_department);
+            cy.get(".MuiTypography-body1").contains(actorDetails.place_of_birth);
+        })
+        it("test the movies actor take part in ", () => {
+            cy.get(".MuiTypography-body1").contains(actorCredits[0].title);
+        })
+    });
+
+    describe("buttons to external link are correct", () => {
+        beforeEach(() => {
+            cy.visit(`/actors/${popular[4].id}`);
+            cy.wait(1000);
+        })
+        it("buttons exist and with correct icons ", () => {
+            cy.wait(1000);
+            if (externalID.facebook_id) {
+                cy.get("svg[data-testid='FacebookIcon']");
+            }
+            if (externalID.twitter_id) {
+                cy.get("svg[data-testid='TwitterIcon']");
+            }
+            if (externalID.instagram_id) {
+                cy.get("svg[data-testid='InstagramIcon']");
+            }
+        })
+    });
+
+    describe("jump to movie detail is correct", () => {
+        beforeEach(() => {
+            cy.visit(`/actors/${popular[4].id}`);
+            cy.wait(1000);
+        })
+        it("test navigate to the actor's detail information ", () => {
+            cy.get(".MuiTypography-body1").contains(actorCredits[0].title).eq(0).click();
+            cy.url().should("include", `/movies/${actorCredits[0].id}`)
+        })
+    });
+});
+
+
--- cypress/e2e/actorList.cy.js
+++ cypress/e2e/actorList.cy.js
@@ -24,15 +24,13 @@ describe("Check actor list", () => { //最外围的主要测试，
     })
 
     describe("dynamic data in actor list is correct", () => {
-
         it("test the length of actor list", () => {
             cy.get(".MuiBox-root").should("have.length", actors.length + 1);//这个总是多出来的一个是谁？
         })
-
         it("test navigate to the actor's detail information ", () => {
             cy.get(".MuiCardContent-root").eq(0).click();
             cy.url().should("include", `/actors/${actors[0].id}`)
-        })//测试跳转的成功不成功
+        })
 
         it("test the name and character of certain actor", () => {
             cy.get(".MuiCardContent-root").eq(0).contains(actors[0].name);
