1fa6f375070c2cc5c212d72003c127850ab1c32c
Fan Yikun <LuMingJun71543@outlook.com>
2022-11-08 13:54:46 +0000

    refactor the query function

--- src/pages/favoriteMoviesPage.js
+++ src/pages/favoriteMoviesPage.js
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import PageTemplate from "../components/templateMovieListPage";
 import { MoviesContext } from "../contexts/moviesContext";
 import { useQueries } from "react-query";
-import { getMovie } from "../api/tmdb-api";
+import { getMovie2 } from "../api/tmdb-api";
 import Spinner from '../components/spinner'
 import RemoveFromFavorites from "../components/cardIcons/removeFromFavorites";
 import WriteReview from "../components/cardIcons/writeReview";
@@ -15,7 +15,7 @@ const FavoriteMoviesPage = () => {
     movieIds.map((movieId) => {
       return {
         queryKey: ["movie", { id: movieId }],
-        queryFn: getMovie,
+        queryFn: getMovie2,
       };
     })
   );
