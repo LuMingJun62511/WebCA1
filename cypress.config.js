4219d2b3e09cc41a63630d428b51161a9600ac09
Fan Yikun <LuMingJun71543@outlook.com>
2022-11-15 09:38:28 +0000

    try refactor component testing

--- cypress.config.js
+++ cypress.config.js
@@ -10,12 +10,12 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-  },
+  }
 
-  component: {
-    devServer: {
-      framework: "create-react-app",
-      bundler: "webpack",
-    },
-  },
+  // component: {
+  //   devServer: {
+  //     framework: "create-react-app",
+  //     bundler: "webpack",
+  //   },
+  // },
 });
